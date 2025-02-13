767af530df710c40d0077cdc639cd6619acceeae
sashenkaaa26 <alexandra.mladinova@gmail.com>
2025-02-13 15:47:44 +0200

    Completed hw from lesson6

---
+++ js-intro-to-functions/task1.js
@@ -0,0 +1,22 @@
+//Function Declaration
+
+// function getRectangleArea(width, height){
+//   let rectangleArea = width*height;
+//   return rectangleArea;
+// }
+
+// console.log(getRectangleArea(13,20))
+
+//Function Expression 
+
+// const getRectangleArea = function (width,height){
+//   let rectangleArea = width*height;
+//   return rectangleArea;
+// }
+// console.log(getRectangleArea(33,54))
+
+
+//Arrow function
+const getRectangleArea = (width, height) => width*height;
+
+console.log(getRectangleArea(3,4))
